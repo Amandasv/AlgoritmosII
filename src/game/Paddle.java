204e34f5649778a616f61511464b8a6d3e50e99c
amandasv <amandasv.pm@gmail.com>
2016-09-18 18:13:07 -0300

    metodospara o paddle

--- src/game/Paddle.java
+++ src/game/Paddle.java
@@ -4,12 +4,31 @@ import com.senac.SimpleJava.Graphics.Color;
 import com.senac.SimpleJava.Graphics.Image;
 import com.senac.SimpleJava.Graphics.Point;
 import com.senac.SimpleJava.Graphics.Rect;
+import com.senac.SimpleJava.Graphics.Resolution;
 import com.senac.SimpleJava.Graphics.Sprite;
 
 public class Paddle extends Sprite {
 
 	public Paddle(int largura) {
 		super(largura,3,Color.GRAY);
+	}
+	
+	public void moveEsquerda(){
+		Rect posicaoTamanhoPaddle = getBounds();
+		if(posicaoTamanhoPaddle.x < 4){
+			super.move(0,0);
+		} else{
+			super.move(-20, 0);
+		}
+	}
+	
+	public void moveDireita(){
+		Rect posicaoTamanhoPaddle = getBounds();
+		if(posicaoTamanhoPaddle.x > Resolution.MSX.width - 34){
+			super.move(0, 0);
+		}else{
+			super.move(20, 0);
+		}
 	}
 	
 	public void colidiu(Bola bola){
